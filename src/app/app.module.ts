a48b157a1ba0cca1c75074c91d9fcf6379575165
bobAscold <ascold@ex.ua>
2017-05-04 07:51:23 +0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { MetaInfoComponent } from './meta-info/meta-info.component';
-import { AlbumItemComponent } from './album-item/album-item.component';
-import { PhotoListItemComponent } from './photo-list-item/photo-list-item.component';
-import { TooltipComponent } from './tooltip/tooltip.component';
-import { PhotoSingleComponent } from './photo-single/photo-single.component';
+import {RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {TabsComponent} from './tabs/tabs.component';
+import {MetaInfoComponent} from './meta-info/meta-info.component';
+import {AlbumItemComponent} from './album-item/album-item.component';
+import {PhotoListItemComponent} from './photo-list-item/photo-list-item.component';
+import {TooltipComponent} from './tooltip/tooltip.component';
+import {PhotoSingleComponent} from './photo-single/photo-single.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignupButtonComponent } from './signup-button/signup-button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TabsComponent,
-    MetaInfoComponent,
-    AlbumItemComponent,
-    PhotoListItemComponent,
-    TooltipComponent,
-    PhotoSingleComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        TabsComponent,
+        MetaInfoComponent,
+        AlbumItemComponent,
+        PhotoListItemComponent,
+        TooltipComponent,
+        PhotoSingleComponent,
+        SignupComponent,
+        SignupButtonComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {path: '', component: AppComponent},
+            {path: 'login', component: SignupComponent}
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
